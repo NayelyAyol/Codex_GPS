3325a246a4ea4f24cbf39e27f985d85242f236fd
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-06-24 19:00:21 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GPS_Ionic',
+  webDir: 'www'
+};
+
+export default config;
